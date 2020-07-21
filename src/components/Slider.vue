56fed3ba84fe4fbe4a6dcc8a5a168db7b9cce676
Sean Daniels <daniels2@mskcc.org>
2020-07-21 16:15:36 -0400

    Fix timings

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -133,7 +133,7 @@ export default {
   transition: all .3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .slide-fade-enter, .slide-fade-leave-to
 /* .slide-fade-leave-active below version 2.1.8 */ {
